709168286146e8a2930f1e7ee50e0f61d5b325e2
Mary Jack <maryjackinthebox@gmail.com>
2018-05-01 21:19:00 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,26 @@
+class CashRegister
+  
+  attr_accessor :total, :discount, :items, :list
 
+  def initialize(discount=0)
+    @total = 0
+    @discount = discount
+    @items =[]
+    @list =[]
+  end
+  
+  def add_item(item, price, quantity)
+    @items << item
+    total = item
+  end
+  
+  def apply_discount
+  end
+  
+  def items
+  end
+
+  def void_last_transaction
+  end
+  
+end
